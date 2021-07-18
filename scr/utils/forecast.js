19a35ece866fecf83539e865fe66f2a16ee74c0d
bishalbhandari125 <52310745+bishalbhandari125@users.noreply.github.com>
2021-07-18 09:19:15 +0545

    Adding a minor detail in the app

--- scr/utils/forecast.js
+++ scr/utils/forecast.js
@@ -10,7 +10,6 @@ const forecast = (latitude,longitude,callback) => {
                 callback('Unable to find location', undefined)
            }
            else{
-                console.log(body.current)
                 callback(undefined, body.current.weather_descriptions + '. It is currently '+  body.current.temperature +'.  degrees out there. The pressure is '+ body.current.pressure +'.  The humidity of the current location is '+body.current.humidity+'.')
            }
         })
